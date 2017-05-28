dbe0d4c245d6c753f480f837edf63e209ef79bcd
Eccenux <eccenux+git@gmail.com>
2017-05-28 23:28:41 +0200

    meta for updates

---
+++ zoom-tools.meta.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @id             iitc-plugin-zoom-tools@eccenux
+// @name           IITC plugin: Zoom tools
+// @category       Misc
+// @version        0.0.1
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @description    [0.0.1] This plugin provides extra toolbar for accurate zoom to view all portals, ale links and whole region.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
